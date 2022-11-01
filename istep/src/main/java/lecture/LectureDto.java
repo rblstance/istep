aab83b9681447d73c16a920775280d916477838c
EZEN <EZEN@DESKTOP-BD28S3H>
2022-11-01 16:25:23 +0900

    11/01 16:25 commit #sw

---
+++ istep/src/main/java/lecture/LectureDto.java
@@ -0,0 +1,74 @@
+package lecture;
+
+import java.sql.Timestamp;
+
+public class LectureDto {
+	
+	// Member
+	private int no;
+	private int sbjCode;
+	private String name;
+	private String thumbnail;
+	private String url;
+	private int time;
+	private Timestamp regDate;
+	
+	// All Arguments Constructor
+	public LectureDto(int no, int sbjCode, String name, String thumbnail, String url, int time, Timestamp regDate) {
+		super();
+		this.no = no;
+		this.sbjCode = sbjCode;
+		this.name = name;
+		this.thumbnail = thumbnail;
+		this.url = url;
+		this.time = time;
+		this.regDate = regDate;
+	}
+
+	// Getter & Setter
+	public int getNo() {
+		return no;
+	}
+
+	public int getSbjCode() {
+		return sbjCode;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getThumbnail() {
+		return thumbnail;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public int getTime() {
+		return time;
+	}
+
+	public Timestamp getRegDate() {
+		return regDate;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setThumbnail(String thumbnail) {
+		this.thumbnail = thumbnail;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+	
+	
+}
---
+++ istep/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>IStep Code Learn</title>
+</head>
+<body>
+	Hello istep
+</body>
+</html>
