f49aa09b5aa4be8f903fbc76e35bb79cfca993b3
EZEN <EZEN@DESKTOP-P8FVMIG>
2022-11-01 18:06:10 +0900

    22-11-01 18:05 AnswerDao BoardDao 뼈대

--- istep/src/main/java/answer/AnswerDao.java
+++ istep/src/main/java/answer/AnswerDao.java
@@ -1,5 +1,31 @@
 package answer;
 
-public class AnswerDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class AnswerDao {
+	private String url = "mysql://database-1.c7ckrqjyxglw.ap-northeast-2.rds.amazonaws.com:3306/istep";
+	private String user = "admin";
+	private String password = "H77LtnHvcj6uYsgEv3ZT";
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private AnswerDao() {
+		this.url = "";
+		this.user = "";
+		this.password = "";
+
+		this.conn = null;
+		this.pstmt = null;
+		this.rs = null;
+	}
+	
+	private static AnswerDao instance = new AnswerDao();
+	
+	public static AnswerDao getInstance() {
+		return instance;
+	}
 }
--- istep/src/main/java/board/BoardDao.java
+++ istep/src/main/java/board/BoardDao.java
@@ -1,5 +1,31 @@
 package board;
 
-public class BoardDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+public class BoardDao {
+	private String url = "mysql://database-1.c7ckrqjyxglw.ap-northeast-2.rds.amazonaws.com:3306/istep";
+	private String user = "admin";
+	private String password = "H77LtnHvcj6uYsgEv3ZT";
+
+	private Connection conn;
+	private PreparedStatement pstmt;
+	private ResultSet rs;
+	
+	private BoardDao() {
+		this.url = "";
+		this.user = "";
+		this.password = "";
+
+		this.conn = null;
+		this.pstmt = null;
+		this.rs = null;
+	}
+	
+	private static BoardDao instance = new BoardDao();
+	
+	public static BoardDao getInstance() {
+		return instance;
+	}
 }
