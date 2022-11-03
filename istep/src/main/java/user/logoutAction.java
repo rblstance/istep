4b6c4345429e7b93bfd3f3151b6abea0f3410cc2
User <User@DESKTOP-G8BDCJJ>
2022-11-03 13:20:58 +0900

    update : 2022/11/03 13:20 회원삭제 회원수정 비밀번호 확인 코드 작성

---
+++ istep/src/main/java/user/logoutAction.java
@@ -0,0 +1,54 @@
+package user;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class logoutAction
+ */
+//@WebServlet("/logoutAction")
+public class logoutAction extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public logoutAction() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		
+		HttpSession session = request.getSession();
+		String log = (String)session.getAttribute("log");
+		
+		if(log != null) {
+			session.invalidate();
+			out.println("<script>alert('로그아웃 되었습니다.');location.href='index';</script>");
+		//response.sendRedirect("index");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
