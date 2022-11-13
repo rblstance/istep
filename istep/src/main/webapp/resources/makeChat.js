8c4231d0b13f4a9f4ad7ff4038bbd473ea52d280
rblstance <115520965+rblstance@users.noreply.github.com>
2022-11-13 13:44:05 +0900

    1113 1344

--- istep/src/main/webapp/resources/makeChat.js
+++ istep/src/main/webapp/resources/makeChat.js
@@ -2,15 +2,15 @@ function makeChannel() {
 
 	var host_id = document.getElementById('host_id').value;
 	var users = [host_id];
-	 $("input[name=users]").each(function(){    
-	    var value = $(this).val();
+	$("input[name=users]").each(function() {
+		var value = $(this).val();
 		users.push(value);
-	  });
+	});
 	var name = document.getElementById('name').value;
 	var custom_type = $('input[name=custom_type]:checked').val();
 	var data = document.getElementById('data').value;
 	var access_code = document.getElementById('access_code').value;
-	
+
 	var settings = {
 		"url": "https://api-C2C776A7-4236-4340-836A-E42E1963FD0E.sendbird.com/v3/group_channels",
 		"method": "POST",
@@ -35,4 +35,26 @@ function makeChannel() {
 	$.ajax(settings).done(function(response) {
 		console.log(response);
 	});
+
+
+	var settings = {
+		"url": "https://api-C2C776A7-4236-4340-836A-E42E1963FD0E.sendbird.com/v3/group_channels/sendbird_group_channel_99443440_3cd4b437c79f861d2bd0b103da7cd610fd2123d0/messages",
+		"method": "POST",
+		"timeout": 0,
+		"headers": {
+			"Api-Token": "92fd33f2bd65a4ab400a443b08a95fb59de5542d",
+			"Content-Type": "application/json",
+			"Accept": "application/json"
+		},
+		"data": JSON.stringify({
+			"message_type": "MESG",
+			"user_id": "409319",
+			"message": "새 채팅방이 생성되었습니다."
+		}),
+	};
+
+	$.ajax(settings).done(function(response) {
+		console.log(response);
+	});
+
 }
